b092e5dc244c55c59de45194f75c1ab94c59ed95
Andrés Martínez <103913163+andresdanielmtz@users.noreply.github.com>
2026-03-30 22:12:31 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SQL-Snippets
+ 
+Snippets I get from [SQL Climber](https://www.sqlclimber.com). 
+
+We'll figure it out. Let's keep on learning!
---
+++ beginner.sql
@@ -0,0 +1,6 @@
+-- Select all the data from a table.
+SELECT * FROM Country;
+
+-- Select specific attributes from a table.
+SELECT Name, Capital FROM Country;
+SELECT Name, Description FROM TouristAttraction;
